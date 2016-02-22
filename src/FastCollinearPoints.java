b6b02e6d4b5de571f9318db551ec3b911b1072ba
Chris Gibb <chrisrgibb@gmail.com>
2016-02-22 21:07:13 +1300

    working

---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,17 @@
+/**
+ * Created by chrisgibb on 22/02/16.
+ */
+public class FastCollinearPoints {
+
+    public FastCollinearPoints(Point[] points){
+
+    }
+
+    public int numberOfSegments(){
+        return  0;
+    }
+
+    public LineSegment[] segments(){
+        return null;
+    }
+}
